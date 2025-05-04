a1beb8fdd935cbf430be077a5373fa09998dae5d
Krishna240517 <codexgame7@gmail.com>
2025-05-05 01:17:14 +0530

    Final - way of interactive Button component with Error handling

--- 01-react-basics/src/App.jsx
+++ 01-react-basics/src/App.jsx
@@ -2,16 +2,28 @@ import { useState } from 'react';
 import './App.css'
 function App() {
   const[count, setCount] = useState(10);
+  const[error, setError] = useState('');
   function onClickUp(){
-    setCount(count + 1);
+    if(count + 1 > 20){
+      setError('Limit : 20 reached');
+    } else {
+      setCount(count + 1);
+      setError('');
+    }
   }
   function onClickDown(){
-    setCount(count - 1);
+    if(count - 1 < 0){
+      setError('Cannot Hit Negative Number')
+    } else {
+      setCount(count - 1);
+      setError(''); 
+    }
   }
   return(
     <>
       <h1>KRISHNA REACTS</h1>
       <Content count={count}/>
+      {error && <p style={{color : "red"}}>{error}</p>}
       <button onClick={onClickUp}>Increase Counter</button>
       <br />
       <button onClick={onClickDown}>Decrease Counter</button>
