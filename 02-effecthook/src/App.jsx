621edf0b41d63bf1dc23deee5c5287b7775dad00
Krishna240517 <codexgame7@gmail.com>
2025-05-07 22:27:02 +0530

    applying useEffect hook

--- 02-effecthook/src/App.jsx
+++ 02-effecthook/src/App.jsx
@@ -1,19 +1,33 @@
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
   const[count,setCount] = useState(0);
 
-  function increaseCounter(){
-    setCount(count + 1);
-  }
+  useEffect(()=>{
+    const id = setInterval(()=>{
+      setCount(function(count){
+        if(count >= 10){
+          clearInterval(id); //clear when reaching limit
+          return 0;
+        } else {
+          return count + 1; 
+        }
+      })
+    },1000)
+    return ()=> clearInterval(id);  //cleanup on unmount
+  },[]);
+
+
   return(
     <Content count={count}/>
   )
 }
 
 function Content(props){
+  return(
   <h1>{props.count}</h1>
+  );
 }
 
 export default App
