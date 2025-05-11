a03904948ad079bd4b7037f85609c000f38854d7
Krishna240517 <codexgame7@gmail.com>
2025-05-11 20:54:41 +0530

    useEffect hook use

--- 02-effecthook/src/App.jsx
+++ 02-effecthook/src/App.jsx
@@ -2,37 +2,46 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const[count,setCount] = useState(0);
-  const[error,setError] = useState('');
+  const[vis, setVis] = useState(true);
   useEffect(()=>{
     const id = setInterval(()=>{
-      setCount(function(count){
-        if(count >= 10){
-          clearInterval(id);
-          setError('Cannot take counter more than 10'); //clear when reaching limit
-          return 0;
-        } else {
-          setError('');
-          return count + 1; 
-        }
-      })
+      setVis(c => !c);
+    },5000)
+    return ()=>clearInterval(id);
+  },[])
+  return(
+    <>
+      {vis && <Counter></Counter>}
+    </>
+  )
+}
+
+
+function Counter(){
+  const [count,setCount] = useState(0);
+  const [error,setError] = useState('');
+
+  //no dependency array
+  useEffect(()=>{
+    const id = setInterval(()=>{
+      if(count + 1 > 10){
+        setError("Cannot Exceed More than 10")
+        clearInterval(id);
+      } else {
+        setCount(count => count + 1);
+        setError("");
+      }
     },1000)
-    return ()=> clearInterval(id);  //cleanup on unmount  
+
+    return () => clearInterval(id);
   },[]);
 
 
   return(
     <>
-      <Content count={count}/>
       {error && <p style={{color : "red"}}>{error}</p>}
+      <h1>{count}</h1>
     </>
   )
 }
-
-function Content(props){
-  return(
-  <h1>{props.count}</h1>
-  );
-}
-
 export default App
